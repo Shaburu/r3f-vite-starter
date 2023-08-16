b0316c5366c55199945a26d031f010f360188a7f
shaburu <xshaburu@gmail.com>
2023-08-16 07:24:12 +0530

    planes and more meshes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,7 @@
 import { OrbitControls , Sky, Environment} from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import {useControls} from 'leva';
+import { MeshStandardMaterial } from "three";
 
 export const Experience = () => {
 
@@ -18,7 +19,7 @@ export const Experience = () => {
       {/* Lobby IS THE BEST - 
 1. apartment
 2. city
-3. dawn
+3. dawn - more pinkish
 4. forest
 5. lobby
 6. night
@@ -30,6 +31,23 @@ export const Experience = () => {
       <group position-y={-1}>
         <Avatar animation={animation}/>
       </group>
+      {animation === 'thank' &&(
+        <mesh scale={0.1}  rotation-x={-Math.PI*0.5} position-z={-0.25} position-y={0.90}>
+      <torusKnotGeometry/>
+      <meshStandardMaterial color='transparent'/>
+    </mesh>
+      )}
+
+      {/* GREEN SCREEN */}
+      {/* <mesh scale={700} rotation-z={-Math.PI*0.5} position-z={-1}>
+        <planeGeometry/>
+        <meshStandardMaterial color='green'/>
+      </mesh> */}
+
+      <mesh scale={7} rotation-x={-Math.PI*0.5} position-y={-0.99}>
+        <planeGeometry/>
+        <meshStandardMaterial color='cyan'/>
+      </mesh> 
     </>
   );
 };
